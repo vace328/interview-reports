c7124d4bea52fa628eeb8eac1226e61169e5d320
NestorPand <pandurovicnestor13@gmail.com>
2024-12-05 16:55:45 +0100

    home page changed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router";
+import Footer from "../../components/Footer/Footer";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <nav>
-        <NavLink to="/candidates">Candidates</NavLink>
-        <button>Login</button>
-      </nav>
-    </div>
+    <>
+      <div>
+        <nav>
+          <NavLink to="/candidates">Candidates</NavLink>
+          <button>Login</button>
+        </nav>
+        <main>
+          <div className="welcome-text">
+            <h1>Welcome Candidates</h1>
+          </div>
+
+          <div className="business-image">
+            <img
+              src="https://thumbs.dreamstime.com/b/business-meeting-silhouette-sunset-37284204.jpg"
+              alt=""
+            />
+          </div>
+        </main>
+      </div>
+      <Footer />
+    </>
   );
 };
 
