585db1e0046bd5870cd8ea649c1360c96fc581ac
badivukm <badivuk.marko32@gmail.com>
2024-12-06 16:25:28 +0100

    search completed, updated app.js and candidates page

--- src/pages/Candidates/Candidates.jsx
+++ src/pages/Candidates/Candidates.jsx
@@ -1,22 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import { candidatesContext } from "../../contexts";
 import "./Candidates.css";
 import Card from "../../components/Card/Card";
+import Search from "../../components/Search/Search";
 
 const Candidates = () => {
   const candidates = useContext(candidatesContext);
+
   console.log(candidates);
 
+  const [search, setSearch] = useState("");
+  const filteredCandidates = candidates?.candidates?.filter((candidate) => {
+    return candidate?.name.toLowerCase().includes(search);
+  });
   return (
     <>
       <div>
         <Header />
         <div className="content-wrapper">
           <h1>Candidates</h1>
+          <Search
+            search={search}
+            setSearch={setSearch}
+            candidates={candidates}
+          />
           <div className="cards-wrapper">
-            {candidates?.candidates?.map((candidate) => {
+            {filteredCandidates?.map((candidate) => {
               return <Card candidate={candidate} />;
             })}
           </div>
