0c76a1e85d668cb3ace5d196b878932ef0d1aa59
Ana <zlatkovic.ana@gmail.com>
2024-12-08 18:02:25 +0100

    footer at the bottom only when the page is short, otherwise, scroll with content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,51 @@
-import React from "react";
-import { NavLink } from "react-router";
+import React, { useState, useRef, useEffect } from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import "./Home.css";
 
 const Home = () => {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [pageWrapperHeight, setPageWrapperHeight] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setPageWrapperHeight(ref.current.clientHeight);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowHeight(window.innerHeight);
+    });
+  }, []);
+
+  // console.log(windowHeight);
+  // console.log(pageWrapperHeight);
+
+  let isShortContent =
+    windowHeight > pageWrapperHeight
+      ? "outer-wrapper shortContent"
+      : "outer-wrapper";
+
   return (
-    <div>
-      <nav>
-        <NavLink to="/candidates">Candidates</NavLink>
-        <button>Login</button>
-      </nav>
+    <div className={isShortContent}>
+      <div className="page-wrapper" ref={ref}>
+        <Header />
+        <div className="content-wrapper">
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+          <p>ghdvdgvdv</p>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
