55fb8230e2771042589f7c0eec780159f007e3ef
dbrkic76 <brkicdragan@hotmail.com>
2024-12-10 11:44:43 +0100

    modal component finished

--- src/components/RCModal/RCModal.jsx
+++ src/components/RCModal/RCModal.jsx
@@ -23,18 +23,28 @@ const RCModal = ({ visible, setVisible, report }) => {
         },
       }}
     >
-      <div>
-        {report.candidateName}
-        <button onClick={() => setVisible(false)}>X</button>
-      </div>
-      <div>
-        <div>
-          <h2>{report.companyName}</h2>
-          <h2>{formatDate(report.interviewDate)}</h2>
-          <h2>{report.phase}</h2>
-          <h2>{report.status}</h2>
+      <div className="fullModal">
+        <div className="modalCandidateName">
+          {report.candidateName}
+          <button onClick={() => setVisible(false)}>X</button>
         </div>
-        <div>{report.note}</div>
+        <div className="modalCandidateInfo">
+          <div className="modalCandidateStatus">
+            <h3>Company:</h3>
+            <h2>{report.companyName}</h2>
+            <h3>Interview Date:</h3>
+            <h2>{formatDate(report.interviewDate)}</h2>
+            <h3>Interview Phase:</h3>
+            <h2>{report.phase}</h2>
+            <h3>Interview Status:</h3>
+            <h2>{report.status}</h2>
+          </div>
+
+          <div className="modalCandidateNote">
+            <h3>Notes:</h3>
+            <div>{report.note}</div>
+          </div>
+        </div>
       </div>
     </Modal>
   );
